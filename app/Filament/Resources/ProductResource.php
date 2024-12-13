66bbdd99b482be124ec23fbb24938bad2d6b285d
AllanPessin <allan.pessin@outlook.com>
2024-12-13 17:16:52 -0300

    feat: slug reativo

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\CategoryProduct;
 use App\Models\Product;
+use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -17,6 +18,8 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\ViewField;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Set;
+use Illuminate\Support\Str;
 
 class ProductResource extends Resource
 {
@@ -28,27 +31,32 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\TextInput::make('title')->required()->label('Título')
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
                 Forms\Components\TextInput::make('slug')->required()->unique(),
                 Section::make([
-                    Forms\Components\DatePicker::make('start_date')->required(),
-                    Forms\Components\DatePicker::make('end_date'),
+                    Forms\Components\DatePicker::make('start_date')
+                        ->required()
+                        ->label('Data início')
+                        ->default(fn(?string $date = null) => $date ? Carbon::now($date) : Carbon::now()),
+                    Forms\Components\DatePicker::make('end_date')->label('Data fim'),
                     Forms\Components\Select::make('category_id')
                         ->relationship(name: 'category', titleAttribute: 'name')
+                        ->label('Categoria')
                         ->required(),
                 ])->columns(3),
-                Forms\Components\RichEditor::make('content')->required()->columnSpanFull(),
-                Forms\Components\Textarea::make('summary')->required()->rows(8)->columnSpanFull(),
+                Forms\Components\RichEditor::make('content')->required()->columnSpanFull()->label('Conteúdo'),
+                Forms\Components\Textarea::make('summary')->required()->rows(8)->columnSpanFull()->label('Resumo'),
             ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->query(CategoryProduct::query())
             ->columns([
                 Tables\Columns\TextColumn::make('title'),
-                Tables\Columns\TextColumn::make('category'),
+                Tables\Columns\TextColumn::make('category.name'),
                 Tables\Columns\TextColumn::make('start_date'),
                 Tables\Columns\TextColumn::make('end_date'),
             ])
